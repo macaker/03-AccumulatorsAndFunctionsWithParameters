2468c528d84766a724d47c06132725dc170a3d3f
macaker <macaker@rose-hulman.edu>
2019-01-30 16:12:02 -0500

    4

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -16,11 +16,11 @@ def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
     print_sequence1(21)
-    #draw_circles1()
+    draw_circles1()
     print_sequence2(0, 18)
-    #draw_circles2()
+    draw_circles2()
     print_sequence3()
-    #draw_circles3()
+    draw_circles3()
     print_cosines()
     draw_cosines_and_sines()
 
